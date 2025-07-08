b13d63a2e30cc0c4f2ad7d76dfbea0ad753e7bf8
Asmau Yusuf Abdullahi <asmauabdullahi19@gmail.com>
2025-07-08 10:16:28 +0100

    First commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const url = 'https://api.adviceslip.com/advice'
+
+
+let adviceId = document.querySelector('.js-advice-id')
+let quoteAdvice = document.querySelector('.js-quote-advice')
+
+async function fetchData() {
+    try {
+        const response = await fetch(url)
+        if(!response.status){
+            throw new Error(
+                'error'
+            )
+        }
+        const data = await response.json()
+        console.log(data)
+        adviceId.innerHTML = data.slip.id
+        quoteAdvice.innerHTML = data.slip.advice
+    } catch (error) {
+        console.error('Error fetching data', error)
+    }
+}
+
+fetchData()
+
+let diceButton = document.querySelector('.js-dice-button')
+diceButton.addEventListener('click', () => {
+    fetchData()
+})
